afc6fa65c8dd26ed4a3ec9de2afb69ad9a42986c
NathanMad <nathan@internit.com.br>
2025-06-30 22:27:52 -0300

    commit 30/06: extracao docx finalizada

--- extract_docx.py
+++ extract_docx.py
@@ -11,12 +11,12 @@ re_extract_date = r"(\d{2}\/\d{2}\/\d{4})"
 
 counter = 0
 
-docx_tables = []
+docx_tables = {}
 docx_tables_data = []
 temp_arr = []
 
 # Load the .docx file
-doc = Document("./amostragem/teste.docx")
+doc = Document("./amostragem/teste6.docx")
 # Print each paragraph
 for para in doc.paragraphs:
     if re.findall(re_find_table_summary, unidecode(para.text), re.IGNORECASE):
@@ -28,42 +28,50 @@ for para in doc.paragraphs:
             summary_structure["sistema"] = re.split(r"^\w+[\-\:\s]+", re.split(r"[\:\-](?=\b\w+\b)", data[i+1], re.IGNORECASE)[0], re.IGNORECASE)[1]
             summary_structure["municipio"] = re.split(r"^\w+[\-\:\s]+", re.split(r"[\:\-](?=\b\w+\b)", data[i+2], re.IGNORECASE)[0], re.IGNORECASE)[1]
             summary_structure["data"] = re.findall(re_extract_date, data[i+3], re.IGNORECASE)
-            data_structure = {"nome": f"cabecalho {counter}", "meses": [], "dados": [], "dados_de_cabecalho": summary_structure}
-            docx_tables.append(data_structure)
-# print(docx_tables[-1])
+            docx_tables[f"cabecalho {counter}"] = { "tabelas": [], "dados_de_cabecalho": summary_structure}
+# print(len(docx_tables))
 counter = 0
 
-for t in doc.tables:
+for t in doc.tables:    #ativa o algoritmo de leitura de tabelas: lê os dados, separa em tabelas e os guarda em uma variável global
     table_rows = []
     for r in t.rows:
         row_data = [cell.text.strip() for cell in r.cells]
         table_rows.append(row_data)
     temp_arr.append(table_rows)
-        
-print(temp_arr)
 
 for t in range(len(temp_arr)):
+    temp_months = []
     for r in range(len(temp_arr[t])):
+        if re.search(re_month, str(temp_arr[t][r]), re.IGNORECASE):
+            temp_months = {cell for cell in temp_arr[t][r] if re.search(re_month, str(cell), re.IGNORECASE)}
+            temp_months = list(temp_months)
         if re.search(r"(3.frequ[eê]ncia)", str(temp_arr[t][r]), re.IGNORECASE):
             temp_arr[t] = temp_arr[t][r+1:]
             break
-        
-# print([len(t) for t in temp_arr])
-# print(temp_arr[0])
+    temp_arr[t] = [temp_months] + [temp_arr[t]]
 
 for t in temp_arr:
-    temp_arr = [r[0] for r in t if r[0]]
+    temp_months = t[0]
+    temp_arr = [r[0] for r in t[1] if r[0]]
     temp_arr = {key: [] for key in temp_arr}
     counter += 1
     data_structure = {"nome": f"tabela {counter}", "dados": temp_arr}
-    for r in range(len(t)):
-        if t[r][0] in temp_arr:
-            data_structure["dados"][t[r][0]] += [cell for cell in t[r][1:]]
+    for r in range(len(t[1])):
+        if t[1][r][0] in temp_arr:
+            data_structure["dados"][t[1][r][0]] += [cell for cell in t[1][r][1:]]
         else:
-            data_structure["dados"][t[r-1][0]] = [data_structure["dados"][t[r-1][0]],t[r][1:]]
+            data_structure["dados"][t[1][r-1][0]] = [data_structure["dados"][t[1][r-1][0]],t[1][r][1:]]
+    data_structure["meses"] = temp_months
     docx_tables_data.append(data_structure)
     
-# print(docx_tables_data[-1])
+decrementer = 0
 
-        
+for x in range(len(docx_tables_data)):
+    if "Dezembro" not in docx_tables_data[x]["meses"]:
+        docx_tables[f"cabecalho {x - decrementer + 1}"]["tabelas"].append(docx_tables_data[x])
+        decrementer += 1
+    else:
+        docx_tables[f"cabecalho {x - decrementer + 1}"]["tabelas"].append(docx_tables_data[x])
+# print(docx_tables["cabecalho 43"]["tabelas"])
+
         
